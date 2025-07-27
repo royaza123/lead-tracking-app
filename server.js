3cd6921760f9c1ae7f96c0d96c677aa9fad0dfea
royaza123 <aetophis@aetophis.com>
2025-07-27 19:29:48 +0300

    Update server.js

--- server.js
+++ server.js
@@ -37,4 +37,5 @@ app.get('/admin', (req, res) => {
   res.json(data);
 });
 
-app.listen(3001, () => console.log('Server running on port 3001'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
